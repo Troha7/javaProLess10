69a4be7c3448e35147380bd1a084ba5da9f12e85
Dmytro Trotsenko <trohae7@gmail.com>
2022-09-24 00:01:28 +0300

    Add Homework Logger

---
+++ src/ua/hillelit/lms/exceptions/LoggerConfigFormatException.java
@@ -0,0 +1,8 @@
+package ua.hillelit.lms.exceptions;
+
+public class LoggerConfigFormatException extends IllegalArgumentException {
+
+  public LoggerConfigFormatException(String s) {
+    super(s);
+  }
+}
--- src/ua/hillelit/lms/logger/FileLoggerConfiguration.java
+++ src/ua/hillelit/lms/logger/FileLoggerConfiguration.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import ua.hillelit.lms.exceptions.LoggerConfigFormatException;
 
 public class FileLoggerConfiguration {
 
@@ -51,25 +52,29 @@ public class FileLoggerConfiguration {
       strArr[i] = strArr[i].trim();
     }
 
+    validateConfigFormat(strArr);
+
     return strArr;
   }
 
-  public FileLoggerConfiguration load() {
-
-    try {
-      String[] configArr = parsingConfigStr(readConfigFile());
-
-      String file = configArr[0];
-      LoggingLevel level = levelConfig(configArr[1].toUpperCase());
-      long maxSize = Long.parseLong(configArr[2]);
-      String format = configArr[3].toUpperCase();
-
-      return new FileLoggerConfiguration(file, level, maxSize, format);
-
-    } catch (FileNotFoundException e) {
-      throw new RuntimeException(e);
+  private void validateConfigFormat(String[] strArr) {
+    String time = "CURRENT_TIME";
+    if (!strArr[3].contains(time)) {
+      throw new LoggerConfigFormatException("FORMAT: " + strArr[3] +
+          " wrong keyword [" + time + "]");
     }
 
+    String level = "LEVEL";
+    if (!strArr[3].contains(level)) {
+      throw new LoggerConfigFormatException("FORMAT: " + strArr[3] +
+          " wrong keyword [" + level + "]");
+    }
+
+    String message = "STRING-MESSAGE";
+    if (!strArr[3].contains(message)) {
+      throw new LoggerConfigFormatException("FORMAT: " + strArr[3] +
+          " wrong keyword [" + message + "]");
+    }
   }
 
   private LoggingLevel levelConfig(String config) {
@@ -81,6 +86,25 @@ public class FileLoggerConfiguration {
         return LoggingLevel.DEBUG;
       default:
         return null;
+    }
+
+  }
+
+  public FileLoggerConfiguration load() {
+
+    try {
+
+      String[] configArr = parsingConfigStr(readConfigFile());
+
+      String file = configArr[0];
+      LoggingLevel level = levelConfig(configArr[1]);
+      long maxSize = Long.parseLong(configArr[2]);
+      String format = configArr[3];
+
+      return new FileLoggerConfiguration(file, level, maxSize, format);
+
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
     }
 
   }
